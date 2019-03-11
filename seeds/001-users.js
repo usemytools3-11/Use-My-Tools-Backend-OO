e6d5be2a9e353db5fae93500e08e65a485c27290
Olivia Osborn <olioz95@gmail.com>
2019-03-11 18:01:27 -0400

    set up seeds

--- seeds/001-users.js
+++ seeds/001-users.js
@@ -1,13 +1,27 @@
+const faker = require("faker");
+
+function createUser() {
+  return {
+    first_name: faker.name.firstName(),
+    last_name: faker.name.lastName(),
+    email: faker.internet.email(),
+    photo_url: faker.image.avatar(),
+    password: "randomPassword"
+  };
+}
+
+userList = [];
+
+for (let i = 1; i < 20; i++) {
+  userList.push(createUser());
+}
+
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex("users")
     .truncate()
     .then(function() {
       // Inserts seed entries
-      return knex("users").insert([
-        { id: 1, colName: "rowValue1" },
-        { id: 2, colName: "rowValue2" },
-        { id: 3, colName: "rowValue3" }
-      ]);
+      return knex("users").insert(userList);
     });
 };
