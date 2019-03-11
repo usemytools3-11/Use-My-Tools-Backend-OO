24bb1e824a6a3a8ba33a19ec5f8ff30116e67ece
Olivia Osborn <olioz95@gmail.com>
2019-03-11 13:19:35 -0400

    filled out usersModel

---
+++ models/usersModel.js
@@ -0,0 +1,34 @@
+db = require("../database/dbConfig");
+
+function get() {
+  return db("users");
+}
+
+function getBy(filter) {
+  return db("users").where(filter);
+}
+
+function getById(id) {
+  return db("users")
+    .where({ id })
+    .first();
+}
+
+async function add(user) {
+  const [id] = await db("users").insert(user);
+  return getById(id);
+}
+
+function remove(id) {
+  return db("coworkers")
+    .where({ id })
+    .del();
+}
+
+module.exports = {
+  get,
+  getBy,
+  getById,
+  add,
+  remove
+};
