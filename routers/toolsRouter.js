38f9737d13cec661a2780f9aa6d7eef059036591
Olivia Osborn <olioz95@gmail.com>
2019-03-12 11:57:06 -0400

    can now get tool by id

--- routers/toolsRouter.js
+++ routers/toolsRouter.js
@@ -11,7 +11,15 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/id", (req, res) => {
-  Tools.getById(req.params.id);
+router.get("/:id", (req, res) => {
+  if (!req.params.id) {
+    res.status(404).json({ message: "Couldn't find user by that ID" });
+  } else {
+    Tools.getById(req.params.id)
+      .then(tool => {
+        res.status(200).json({ tool });
+      })
+      .catch(error => res.status(500).json({ error: "couldn't fetch data" }));
+  }
 });
 module.exports = router;
