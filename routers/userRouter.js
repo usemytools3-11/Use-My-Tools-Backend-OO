fba5a82cd898980b7d7bfbcce89bc2e42a939662
Olivia Osborn <olioz95@gmail.com>
2019-03-13 13:13:34 -0400

    added ability to get users by id

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -11,6 +11,20 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await Users.getById(req.params.id);
+
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "user with that ID does not exist!" });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 router.put("/:id", async (req, res) => {
   if (
     !req.body.first_name ||
